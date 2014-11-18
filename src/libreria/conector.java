3cf0a8d5ffeca5d3c38ddd1ced368e1f2eb6a702
TonyBang <tonybang@live.com.mx>
2014-11-18 13:17:26 +0100

    Funcion de qwerty
    Se crea la funcion ejecutaqwerty la cual ejecuta directamente una
    sentencia para motivos de testing.

--- src/libreria/conector.java
+++ src/libreria/conector.java
@@ -2,15 +2,20 @@ package libreria;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class conector {
-/*
- * Atributos de la clase
- */
+
+	/*
+	 * Atributos de la clase
+	 */
 	Connection conexion;
-/*
- * Constructor
- */
+	private static final int RESULTADOSMAX = 99;
+
+	/*
+	 * Constructor
+	 */
 	public conector(String loginUsuario, String loginContraseña, String urlBD) {
 		// Paso 1: Obtener valores de la interfaz grafica.
 		String[] datosConexion = new String[3];
@@ -19,17 +24,21 @@ public class conector {
 		datosConexion[2] = urlBD;
 		// Paso 2: Obtener el objeto conexion.
 		conexion = dameConexion(datosConexion);
-		System.out.println("Conectado a la BD correctamente! :D");
+		if (conexion == null) {
+			System.out.println("Algo salio mal... :(");
+		} else {
+			System.out.println("Conectado a la BD correctamente! :D");
+		}
 	}
 
-/*
- * Funcion "getter" que devuelve un objeto de tipo "Connection" proporcionado
- * por la libreria DriverManager la cual administra los drivers para conectarse
- * a los diferentes motores de base de datos.
- * Este sistema usa por default mySQL.
- * El arreglo de datos de conexion en 0=url de la BD, 1=usuario y 2=contraseña.
- * NOTA: Siempre al usar esta funcion, validar que el objeto "conexion" sea diferente a NULL.
- */
+	/*
+	 * Funcion "getter" que devuelve un objeto de tipo "Connection"
+	 * proporcionado por la libreria DriverManager la cual administra los
+	 * drivers para conectarse a los diferentes motores de base de datos. Este
+	 * sistema usa por default mySQL. El arreglo de datos de conexion en 0=url
+	 * de la BD, 1=usuario y 2=contraseña. NOTA: Siempre al usar esta funcion,
+	 * validar que el objeto "conexion" sea diferente a NULL.
+	 */
 	public Connection dameConexion(String[] datosConexion) {
 		Connection conexion = null;
 		if (datosConexion.length == 3) {// no vaya a ser que me estes mandando
@@ -71,4 +80,42 @@ public class conector {
 		return conexion;
 	}
 
+	/*
+	 * Funcion basica y creada para el testing y desarrollo del sistema. Se
+	 * ingresa directamente el querty como cadena y regresando cualquier valor a
+	 * cadena. NOTA: solo regresa el valor de 1 columna.
+	 */
+	public String[] ejecutaQWERTY(String qwerty) {
+		java.sql.Statement statement = null;
+		try {
+			statement = conexion.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out
+					.println("Error en funcion ejecutaQWERTY: error en la creacion de statement;");
+		}
+		// inicializacion de los resultados.
+		ResultSet resultados = null;
+		// se ejecuta el qwerty:
+		try {
+			resultados = statement.executeQuery(qwerty);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out
+					.println("Error en funcion ejecutaQWERTY: el qwerty parece estar mal escrito.");
+		}
+		// se inicializa variable de retorno
+		String resultado[] = new String[RESULTADOSMAX];
+		// Se almacenan los resultados en la variable de retorno.
+		try {
+			for (int i = 0; resultados.next(); i++) {
+				resultado[i] = resultados.getString(0);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out
+					.println("Error en funcion ejecutaQWERTY: No se pudo almacenar el resultado en variable.");
+		}
+		return resultado;
+	}
 }
